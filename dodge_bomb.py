1592d91a009c859fb71d63777b89440794a0c603
omochio <omochiodev@gmail.com>
2023-04-25 13:52:37 +0900

    練習問題1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,6 +8,9 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bomb_img = pg.Surface((20, 20))
+    pg.draw.circle(bomb_img, color=(255, 0, 0), center=(0, 0), radius=10)
+    bomb_img.set_colorkey((0, 0, 0))
     tmr = 0
 
     while True:
@@ -18,6 +21,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        # screen.blit(bomb_img, [0, 0])
 
         pg.display.update()
         clock.tick(1000)
